25de38817d697d25f6cf6e2eca32c13a9e8822db
Chan Woo D Ang <cda23@afsaccess1.njit.edu>
2020-02-19 16:24:33 -0500

    polished css

--- rec.php
+++ rec.php
@@ -3,13 +3,15 @@
 $user = $_REQUEST["u"];
 $pass = $_REQUEST["p"];
 
-echo "<br>";
-echo "username: " .  $user . "<br>";
-echo "password: " .  $pass . "<br>";
-
 $hash = sha1($pass);
 
-echo "hashed password: " . $hash;
+echo "<br><br>";
+echo "<table id='info'>";
+echo "<caption>INPUT</caption>";
+echo "<tr><td>username</td><td>$user</td></tr>";
+echo "<tr><td>password</td><td>$pass</td></tr>";
+echo "<tr><td>hashed password</td><td>$hash</td></tr>";
+echo "</table>";
 
 function curl($url) {
 
@@ -27,10 +29,13 @@ function curl($url) {
 
 $url = "https://web.njit.edu/~co77/cs490/alpha/index.php";
 $json = curl($url);
-echo "<br><br>OUTPUT<br> " . "status: " . $json->status . "<br>message: " . $json->message;
+
+echo "<br><br>";
+echo "<table id='info'>";
+echo "<caption>OUTPUT</caption>";
+echo "<tr><td>status</td><td>$json->status</td></tr>";
+echo "<tr><td>message</td><td>$json->message</td></tr>";
+echo "</table>";
 
 curl_close($ch);
-
-
-
 ?>
