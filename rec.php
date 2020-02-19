7e0bc46a7c946a8f34cfdb71b388d703896eecd0
Chan Woo D Ang <cda23@afsaccess1.njit.edu>
2020-02-18 19:10:28 -0500

    added curl as function

--- rec.php
+++ rec.php
@@ -10,16 +10,23 @@ $hash = sha1($pass);
 
 echo "hashed password: " . $hash;
 
-$ch = curl_init();
+function curl($url) {
+
+    global $user;
+    global $hash;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "u=".$user."&p=".$hash);
+    $output = curl_exec($ch);
+    return json_decode($output);
+}
+
 $url = "https://web.njit.edu/~co77/cs490/alpha/index.php";
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "u=".$user."&p=".$hash);
-
-$output = curl_exec($ch);
-
-echo "<br>output: " . $output;
+$json = curl($url);
+echo "<br>output: " . $json->status . $json->message;
 
 curl_close($ch);
 
