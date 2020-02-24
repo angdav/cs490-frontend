9e4c964e934d809251bb0d6ba9dbe60350d0b3a5
Chan Woo D Ang <cda23@afsaccess2.njit.edu>
2020-02-24 14:40:52 -0500

    alpha done

--- rec.php
+++ rec.php
@@ -1,40 +1,38 @@
 <?php
 
-$user = $_REQUEST["u"];
-$pass = $_REQUEST["p"];
-
-$hash = sha1($pass);
+$user = $_POST["u"];
+$pass = $_POST["p"];
 
 echo "<br><br>";
 echo "<table id='info'>";
 echo "<caption>INPUT</caption>";
 echo "<tr><td>username</td><td>$user</td></tr>";
-echo "<tr><td>password</td><td>$pass</td></tr>";
-echo "<tr><td>hashed password</td><td>$hash</td></tr>";
+echo "<tr><td>password</td><td>".strlen($pass)." characters</td></tr>";
 echo "</table>";
 
 function curl($url) {
 
     global $user;
-    global $hash;
+    global $pass;
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "u=".$user."&p=".$hash);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "u=".$user."&p=".$pass);
     $output = curl_exec($ch);
     return json_decode($output);
 }
 
-$url = "https://web.njit.edu/~co77/cs490/alpha/index.php";
+$url = "https://web.njit.edu/~mba27/cs490/stuff.php";
 $json = curl($url);
 
 echo "<br><br>";
 echo "<table id='info'>";
 echo "<caption>OUTPUT</caption>";
 echo "<tr><td>status</td><td>$json->status</td></tr>";
-echo "<tr><td>message</td><td>$json->message</td></tr>";
+echo "<tr><td>db result</td><td>$json->db</td></tr>";
+echo "<tr><td>njit result</td><td>$json->njit</td></tr>";
 echo "</table>";
 
 curl_close($ch);
