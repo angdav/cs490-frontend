fce5a836f6ca16da5d3521f5efce25b1fa6f284c
Chan Woo D Ang <cda23@afsaccess1.njit.edu>
2020-03-03 22:27:23 -0500

    added some frontend, user validation is functioning

--- rec.php
+++ rec.php
@@ -3,12 +3,14 @@
 $user = $_POST["u"];
 $pass = $_POST["p"];
 
+/*
 echo "<br><br>";
 echo "<table id='info'>";
 echo "<caption>INPUT</caption>";
 echo "<tr><td>username</td><td>$user</td></tr>";
 echo "<tr><td>password</td><td>".strlen($pass)." characters</td></tr>";
 echo "</table>";
+*/
 
 function curl($url) {
 
@@ -26,13 +28,23 @@ function curl($url) {
 
 $url = "https://web.njit.edu/~mba27/cs490/connect.php";
 $json = curl($url);
+$db = json_decode($json->db);
 
 echo "<br><br>";
 echo "<table id='info'>";
 echo "<caption>OUTPUT</caption>";
-echo "<tr><td>db result</td><td>$json->db</td></tr>";
-echo "<tr><td>njit result</td><td>$json->njit</td></tr>";
+echo "<tr><td>status</td><td>$json->status</td></tr>";
+echo "<tr><td>username</td><td>$db->username</td></tr>";
+echo "<tr><td>account type</td><td>$db->account_type</td></tr>";
+echo "<tr><td>professor</td><td>$db->professor</td></tr>";
 echo "</table>";
+print_r($db);
+
+if ($json->status == 200 && $db->account_type == "1"){
+    echo "Successful Professor Login! Redirecting...";
+} else if ($json->status == 200 && $db->account_type == "0"){
+    echo "Successful Student Login! Redirecting...";
+}
 
 curl_close($ch);
 ?>
