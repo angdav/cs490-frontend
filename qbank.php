0809851a640be9e8a3628b3052f4616c27d4747d
Chan Woo D Ang <cda23@afsaccess1.njit.edu>
2020-03-08 00:37:32 -0500

    working add question front-end, awaiting proper database insertion

---
+++ qbank.php
@@ -0,0 +1,35 @@
+<?php
+
+function curl($url) {
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "u=".$user."&p=".$pass);
+    $output = curl_exec($ch);
+    return json_decode($output);
+}
+
+$url = "https://web.njit.edu/~mba27/cs490/connect.php";
+$json = curl($url);
+$db = json_decode($json->db);
+
+echo "<br><br>";
+echo "<table id='info'>";
+echo "<caption>OUTPUT</caption>";
+echo "<tr><td>status</td><td>$json->status</td></tr>";
+echo "<tr><td>username</td><td>$db->username</td></tr>";
+echo "<tr><td>account type</td><td>$db->account_type</td></tr>";
+echo "<tr><td>professor</td><td>$db->professor</td></tr>";
+echo "</table>";
+print_r($db);
+
+if ($json->status == 200 && $db->account_type == "1"){
+    echo "Successful Professor Login! Redirecting...";
+} else if ($json->status == 200 && $db->account_type == "0"){
+    echo "Successful Student Login! Redirecting...";
+}
+
+curl_close($ch);
+?>
